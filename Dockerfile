914504f4b64d0a35c6f4c26482cd7325ab2c3951
flameshikari <mail@hexed.pw>
2024-07-22 03:42:33 +0500

    big update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
-FROM erseco/alpine-php-webserver:latest
+FROM node:20-slim AS build
+WORKDIR /app
+COPY --chown=node package.json .
+RUN npm install -d --no-package-lock
+COPY . .
+RUN npm run build
+
+FROM erseco/alpine-php-webserver:latest AS release
 RUN rm -rf /var/www/html/* && \
-    echo 'short_open_tag = On' >> /etc/php81/conf.d/custom.ini && \
+    echo 'short_open_tag = On' >> /etc/php83/conf.d/custom.ini && \
     sed -i 's|$uri/|$uri/ $uri.html|; s|fastcgi_index.*|expires 7d;|; s/ico|xml/ico|xml|txt/' /etc/nginx/nginx.conf
 
-COPY --chown=nobody:nobody ./public/. /var/www/html
+COPY --chown=nobody:nobody --from=build /app/public/. /var/www/html
---
+++ client/index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+import esbuild from 'esbuild';
+import esbuildBabel from '@chialab/esbuild-plugin-babel';
+import esbuildBrowserlist from 'browserslist-to-esbuild';
+import htmlPlugin from '@chialab/esbuild-plugin-html';
+
+
+const babelOpts = {
+    presets: [
+        [
+            '@babel/preset-env', {
+                useBuiltIns: 'usage',
+                corejs: '3.37.1',
+                modules: false,
+            },
+        ],
+    ],
+};
+
+const esbuildOpts = {
+    target: esbuildBrowserlist(),
+    minify: false,
+    outdir: 'public',
+    splitting: true,
+    loader: {
+        '.ttf': 'dataurl',
+        '.png': 'dataurl',
+        '.gif': 'dataurl',
+    },
+    format: 'esm',
+    bundle: true,
+    treeShaking: true,
+    write: true,
+    assetNames: '[name]',
+    chunkNames: 'assets/[name]-[hash]',
+};
+
+const minifyOptions = {
+    collapseBooleanAttributes: true,
+    collapseWhitespace: true,
+    decodeEntities: true,
+    html5: false,
+    minifyURLs: true,
+    removeComments: true,
+    removeAttributeQuotes: true,
+    removeEmptyAttributes: true,
+    removeRedundantAttributes: true,
+    sortClassName: true,
+    sortAttributes: true,
+};
+
+
+await esbuild.build({
+    ...esbuildOpts,
+    entryPoints: ['./client/index.html'],
+    plugins: [
+        esbuildBabel(babelOpts),
+        htmlPlugin({
+            minifyOptions: minifyOptions,
+        }),
+    ],
+});
